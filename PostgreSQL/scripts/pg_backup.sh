c3ed468842cc50b3d8b55fc7f84c990f96032701
Jaimin Pan <jaimin.pan@gmail.com>
2017-10-19 16:21:39 +0800

    Update pg_backup.sh

--- PostgreSQL/scripts/pg_backup.sh
+++ PostgreSQL/scripts/pg_backup.sh
@@ -3,9 +3,9 @@
 TARGET_DAY=`date -d '0 days' +%Y%m%d`
 
 
-TARGET_DIR=/data/appdatas/pg_base/base${TARGET_DAY}
+TARGET_DIR=/data/appdatas/pg_base/base_${TARGET_DAY}
 LOG_DIR=/data/applogs/pg_base/${TARGET_DAY}.log
 
 if [ ! -d "${TARGET_DIR}" ]; then
-    mkdir "${TARGET_DIR}" && pg_basebackup -D "${TARGET_DIR}" -Ft -Xf -v -P -h 10.81.5.1 -p 15432 > $LOG_DIR 2>&1
+    mkdir "${TARGET_DIR}" && pg_basebackup -D "${TARGET_DIR}" -Ft -Xf -v -P -h 10.81.5.1 -p 15432 >> $LOG_DIR 2>&1
 fi
